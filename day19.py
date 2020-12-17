5722dcff0925baead93a28cd4bdef2501fbc4523
Dennis Kindervater <kindervater@picture-gmbh.de>
2020-12-17 22:19:30 +0100

    prep

---
+++ day19.py
@@ -0,0 +1,19 @@
+import re
+import numpy as np
+
+
+def read():
+    return [line for line in open("day19.txt")]
+
+
+def day19_1():
+    pass
+
+
+def day19_2():
+    pass
+
+
+if __name__ == "__main__":
+    day19_1()
+    day19_2()
