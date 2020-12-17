cfeffe72415a13c08563c0c239d0ff1ccbc6ad25
Dennis Kindervater <kindervater@picture-gmbh.de>
2020-12-17 08:54:11 +0100

    What is this?

--- day15.py
+++ day15.py
@@ -1,12 +1,25 @@
 import re
 
-def day15_1():
-    pass
+def day15(idx):
+    nums = {}
+    last_dif = 0
+    cur_num = 0
+    turn = 0
+    
+    for line in open("day15.txt").readline().strip().split(","):
+        turn += 1
+        cur_num = int(line)
+        last_dif = turn - nums.get(cur_num, turn)
+        nums[cur_num] = turn
 
-def day15_2():
-    pass
+    while turn < idx:
+        turn += 1
+        cur_num = last_dif
+        last_dif = turn - nums.get(cur_num, turn)
+        nums[cur_num] = turn
 
+    print(cur_num)
 
 if __name__ == "__main__":
-    day15_1()
-    day15_2()
+    day15(2020)
+    day15(30000000)
